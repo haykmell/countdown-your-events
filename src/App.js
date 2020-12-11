fe621a3c6b0769a2fa7533d3cf2320525019826a
haykmell <meliqyanhayko@yandex.ru>
2020-12-11 23:47:05 +0400

    fix bugs

--- src/App.js
+++ src/App.js
@@ -83,6 +83,57 @@ function App() {
       idTimeOut = setTimeout(() => {
         setEventList((eventList) => {
           return eventList.map((item) => {
+            const calculateDate = (event) => {
+              let realDate = "";
+              let realName = "";
+              let realId = "";
+              if (event === undefined) {
+                realDate = date;
+                realName = eventName;
+                realId = Date.now();
+              } else {
+                realDate = event.eventDate;
+                realName = event.eventName;
+                realId = event.id;
+              }
+              const currentDate = new Date().getTime();
+              const eventDate = new Date(realDate).getTime();
+              const distance = eventDate - currentDate;
+
+              const daysLeft = Math.floor(distance / (1000 * 60 * 60 * 24));
+              const hoursLeft = Math.floor(
+                (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+              );
+              const minutesLeft = Math.floor(
+                (distance % (1000 * 60 * 60)) / (1000 * 60)
+              );
+              const secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
+
+              let timeLeft = {
+                id: realId,
+                eventName: realName,
+                eventDate: realDate,
+                isDone: false,
+                date: {
+                  days: daysLeft,
+                  hours: hoursLeft,
+                  minutes: minutesLeft,
+                  seconds: secondsLeft,
+                },
+                dateRadius: [
+                  mapNumber(daysLeft, 30, 0, 0, 360),
+                  mapNumber(hoursLeft, 24, 0, 0, 360),
+                  mapNumber(minutesLeft, 60, 0, 0, 360),
+                  mapNumber(secondsLeft, 60, 0, 0, 360),
+                ],
+              };
+
+              if (distance < 0) {
+                timeLeft.isDone = true;
+              }
+              return timeLeft;
+            };
+
             return calculateDate(item);
           });
         });
@@ -90,7 +141,7 @@ function App() {
     }
 
     return () => clearTimeout(idTimeOut);
-  }, [eventList]); // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [eventList, date, eventName]); // eslint-disable-next-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     localStorage.setItem("events", JSON.stringify(eventList));
